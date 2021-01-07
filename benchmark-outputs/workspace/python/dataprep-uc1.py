f9bcc809333a5640c8dd31d99af49a67d935e88f
Uluer Emre Ozdil <ulueremre@gmail.com>
2021-01-07 12:07:04 +0300

    fix IO data (bread)

--- benchmark-outputs/workspace/python/dataprep-uc1.py
+++ benchmark-outputs/workspace/python/dataprep-uc1.py
@@ -56,10 +56,12 @@ for data_scale in data_scales:
                 df_io = pd.read_csv('../../uc1/'+csp+'/'+data_scale+'/sysstat/data-w' + str(worker) + '/'+csp+'-uc1-w' + str(worker) + '-'+data_scale+'-'+benchmark+'-i.dat', decimal=",", delim_whitespace=True, header=None)
                 df_io = pd.DataFrame(df_io)
                 df_io.columns = ['time', 'tps', 'rtps', 'wtps', 'bread/s', 'bwrtn/s']
-                x_ioread = df_io[['rtps']].values.astype(float)
+                #x_ioread = df_io[['rtps']].values.astype(float)
+                x_ioread = df_io[['bread/s']].values.astype(float)
                 x_ioread_scaled = scaler.fit_transform(x_ioread)
                 df_ioread_normalized = pd.DataFrame(x_ioread_scaled)
-                x_iowrite = df_io[['wtps']].values.astype(float)
+                #x_iowrite = df_io[['wtps']].values.astype(float)
+                x_iowrite = df_io[['bwrtn/s']].values.astype(float)
                 x_iowrite_scaled = scaler.fit_transform(x_iowrite)
                 df_iowrite_normalized = pd.DataFrame(x_iowrite_scaled)
 
